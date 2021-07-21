c477d0e76e8dec3c7d4de5ca55f5295ffd5fadf0
ckkonggt <87761661+ckkonggt@users.noreply.github.com>
2021-07-22 01:34:13 +0200

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO student VALUES (1, 'James', 'Carter');
+INSERT INTO student VALUES (2, 'Helen', 'Leary');
+INSERT INTO student VALUES (3, 'Linda', 'Douglas');
+INSERT INTO student VALUES (4, 'Rafael', 'Ortega');
+INSERT INTO student VALUES (5, 'Henry', 'Stevens');
+INSERT INTO student VALUES (6, 'Sharon', 'Jenkins');
