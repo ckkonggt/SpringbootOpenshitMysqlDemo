b547abf998bab712733562aec9672f7748b8a704
ckkonggt <87761661+ckkonggt@users.noreply.github.com>
2021-07-22 23:34:18 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,4 @@ INSERT INTO student VALUES (3, 'Linda', 'Douglas');
 INSERT INTO student VALUES (4, 'Rafael', 'Ortega');
 INSERT INTO student VALUES (5, 'Henry', 'Stevens');
 INSERT INTO student VALUES (6, 'Sharon', 'Jenkins');
+INSERT INTO student VALUES (7, 'Peter', 'Pan');
